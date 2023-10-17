4d4c73592525ff8d949a1c1c6354123501635374
root <146715401+Hache-David@users.noreply.github.com>
2023-10-17 02:29:21 -0700

    V3.0

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <unistd.h>
+/**
+ * _islower - Entry point.
+ *
+ * @c : argument int received by the function.
+ *
+ * Return: Always 0.
+ */
+
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
