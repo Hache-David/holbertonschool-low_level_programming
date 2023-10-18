c8530c4304d90d10b838667041e91e0ffaf74bfe
root <146715401+Hache-David@users.noreply.github.com>
2023-10-18 06:27:31 -0700

    V9.4

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -7,9 +7,7 @@
 */
 void times_table(void)
 {
-	int i1 = 0;
-	int i2 = 0;
-	int i3;
+	int i1, i2, i3;
 
 	while (i1 <= 9)
 	{
@@ -20,38 +18,31 @@ void times_table(void)
 			{
 				write(1, "  ", 2);
 				_putchar(i3 + '0');
-				write(1, "\n", 1);
-			}
+				write(1, "\n", 1); }
 			else if (i2 < 9 && i3 < 10 && i2 != 0)
 			{
 				write(1, "  ", 2);
 				_putchar(i3 + '0');
-				write(1, ",", 1);
-			}
+				write(1, ",", 1); }
 			else if (i2 < 9 && i3 < 10 && i2 == 0)
 			{
 				_putchar(i3 + '0');
-				write(1, ",", 1);
-			}
+				write(1, ",", 1); }
 			else if (i2 < 9 && i3 >= 10)
 			{
 				write(1, " ", 1);
 				_putchar((i3 / 10) + '0');
 				_putchar((i3 % 10) + '0');
-				write(1, ",", 1);
-			}
+				write(1, ",", 1); }
 			else if (i2 == 9 && i3 >= 10)
 			{
 				write(1, " ", 1);
 				_putchar((i3 / 10) + '0');
 				_putchar((i3 % 10) + '0');
-				write(1, "\n", 1);
-			}
+				write(1, "\n", 1); }
 			i2++;
 		}
 		i1++;
 		i2 = 0;
 	}
 }
-
-
