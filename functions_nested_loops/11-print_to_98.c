286c48c465b1e730aeeda411e6a50e7839c67996
root <146715401+Hache-David@users.noreply.github.com>
2023-10-19 05:19:29 -0700

    V12.5

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -1,10 +1,9 @@
 #include "main.h"
-#include <unistd.h>
+#include <stdio.h>
 /**
  * print_to_98 - Entry point
  *
- *
- *
+ * @n : index
  *
  * return : (succes) always 0.
  */
@@ -13,77 +12,13 @@ void print_to_98(int n)
 {
 	int i;
 
-	i = n;
-	
-	while (i != 98)
+	for (i = n; i > 98; i--)
 	{
-		if (i >= 98)
-		{
-			if (i >= 10 && i < 100)
-			{
-				_putchar((i / 10) + '0');
-				_putchar((i % 10) + '0');
-				write(1, ",", 1);
-				write(1, " ", 1);
-			}
-			else
-			{
-				_putchar((i / 100) + '0');
-				_putchar(((i % 100) / 10) + '0');
-				_putchar((i % 10) + '0');
-				write(1, ",", 1);
-				write(1, " ", 1);
-			}
-			i--;
-		}
-		else
-		{
-			if (i >= 10)
-			{
-				if (i < 0)
-				{
-					if (i <= -100)
-					{
-						write(1, "-", 1);
-						_putchar((-i / 100) + '0');
-						_putchar(((-i % 100) / 10) + '0');
-						_putchar((-i % 10) + '0');
-						write(1, ",", 1);
-						write(1, " ", 1);
-					}
-					else if(i > -100)
-					{	
-						write(1 , "-", 1);
-						_putchar((-i / 10) + '0');
-						_putchar((-i % 10) + '0');
-						write(1, ",", 1);
-					}
-				}
-				else
-				{
-					_putchar((i / 10) + '0');
-					_putchar((i % 10) + '0');
-					write(1, ",", 1);
-				}
-				write(1, " ", 1);
-			}
-			if (i < 10 && i > -10)
-			{
-				if (i < 0)
-				{
-					write(1 , "-", 1);
-					_putchar(-i + '0');
-				}
-				else
-				{
-					_putchar(i + '0');
-				}
-				write(1, ",", 1);
-				write(1, " ", 1);
-			}
-			i++;
-		}
+		printf("%d, ", i);
 	}
-	write(1 , "98", 2);
-	write(1, "\n", 1);
+	for (i = n; i < 98; i++)
+	{
+		printf("%d, ", i);
+	}
+	printf("98\n");
 }
