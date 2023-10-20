6c3370dca6cc1ddcd7c6ff62f031947c4c6dfc7d
root <146715401+Hache-David@users.noreply.github.com>
2023-10-20 05:51:07 -0700

    V3.1

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_numbers - check the code
+ *
+ * Return: Always 0.
+ */
+
+void print_numbers(void)
+{
+	char c = '0';
+
+	while (c <= '9')
+	{
+		_putchar(c);
+		c++;
+	}
+	_putchar('\n');
+}
