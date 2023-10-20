f08617a0b02dac314b07aad15fc05f90f394497f
root <146715401+Hache-David@users.noreply.github.com>
2023-10-20 07:58:38 -0700

    V9.4

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -10,7 +10,7 @@ int main(void)
 {
 	int i = 1;
 
-	while (i <= 100)
+	while (i < 100)
 	{
 		if ((i % 3) == 0 && (i % 5) != 0)
 		{
@@ -28,6 +28,6 @@ int main(void)
 			printf("%d ", i);
 		i++;
 	}
-	printf("\n");
+	printf("Buzz\n");
 	return (0);
 }
