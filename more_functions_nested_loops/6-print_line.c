7ea6b5840d37cfff286eb1086fbb147f0774c476
root <146715401+Hache-David@users.noreply.github.com>
2023-10-20 06:31:06 -0700

    V6.0

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_line - check the code
+ *
+ * @n: input argt.
+ *
+ * Return: Always 0.
+ */
+
+void print_line(int n)
+{
+	int j;
+
+	for (j = 0; j < n; j++)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+}
