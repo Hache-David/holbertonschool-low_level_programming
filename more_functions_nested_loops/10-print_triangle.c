e49b804b3b6b257301760af6f10b0e816030f394
root <146715401+Hache-David@users.noreply.github.com>
2023-10-20 10:03:49 -0700

    V10.0

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * print_triangle - check the code
+ *
+ * @size: first argt.
+ *
+ * Return: Always 0.
+ */
+
+void print_triangle(int size)
+{
+
+	int i, index, index1;
+
+	if (size == 0)
+	{
+		_putchar('\n');
+	}
+	for (i = 1; i <= size ; i++)
+	{
+		index1 = 0;
+		index = (size - i);
+		while (index1 <= index)
+		{
+			_putchar(' ');
+			index1++;
+		}
+		while (index1 > index && index1 <= size)
+		{
+			_putchar('#');
+			index1++;
+		}
+		_putchar('\n');
+	}
+}
