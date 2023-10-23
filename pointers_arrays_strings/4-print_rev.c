78a20b6bc0a5028b4c2e23110c55430b83d7c8a4
root <146715401+Hache-David@users.noreply.github.com>
2023-10-23 04:47:54 -0700

    V4.0

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_rev - check the code
+ *
+ * @s : input.
+ *
+ * Return: Always 0.
+ */
+
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (*s != '\0')
+	{
+		s++;
+		i++;
+	}
+	while (i != 0)
+	{
+		_putchar(*s);
+		s--;
+		i--;
+	}
+	_putchar('\n');
+}
