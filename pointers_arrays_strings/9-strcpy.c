cb5ea9f660b0fd20399f63c993951e1c9022b90e
root <146715401+Hache-David@users.noreply.github.com>
2023-10-24 00:50:28 -0700

    V9.0

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * *_strcpy - check the code
+ *
+ * @dest: destination.
+ * @src: source.
+ *
+ * Return: Always 0.
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+	int n = 0;
+
+	while (src[n] != '\0')
+	{
+		n++;
+	}
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[i] = src[i];
+	for ( ; i < n; i++)
+		dest[i] = '\0';
+
+	return (dest);
+}
