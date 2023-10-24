8f73c754f51e84eda0288a8be2beb24f882909fa
root <146715401+Hache-David@users.noreply.github.com>
2023-10-24 07:04:20 -0700

    V10.3

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -11,40 +11,44 @@
 
 int _atoi(char *s)
 {
-	int i = 0;
-	int nbr = 0;
+	int i = 0, c = 0, d = 0, stop = 0, nbr = 0;
 	unsigned int sum = 0;
 	int index = 1;
-	int c = 0;
-	int stop = 0;
-	
+
 	if (s[i] == '\0')
 		return (0);
-	while (s[i] != '\0' || stop != 1)
+	while (s[i] != '\0' || d != 1)
 	{
-		if (s[i] == 45)
-			index = index * (-1);
 		if (s[i] >= 48 && s[i] <= 57)
-		{
-			while (s[i] >= 48 && s[i] <= 57)
-			{
-				nbr = s[i] - '0';
-				if (c > 1)
-				{
-				sum = (sum * 10) + nbr;
-				}
-				else
-				{
-					sum = nbr;
-					c = 2;
-				}
-				i++;
-			}
-			return (sum * index);
-			stop = 1;
-		}
+			d = 1;
 		i++;
 	}
-			return (0);
-
+	i = 0;
+	if (d == 1)
+	{
+		while (s[i] != '\0' || stop != 1)
+		{
+			if (s[i] == 45)
+				index = index * (-1);
+			if (s[i] >= 48 && s[i] <= 57)
+			{
+				while (s[i] >= 48 && s[i] <= 57)
+				{
+					nbr = s[i] - '0';
+					if (c > 1)
+					sum = (sum * 10) + nbr;
+					else
+					{
+						sum = nbr;
+						c = 2;
+					}
+					i++;
+				}
+				return (sum * index);
+				stop = 1;
+			}
+			i++;
+		}
+	}
+		return (0);
 }
