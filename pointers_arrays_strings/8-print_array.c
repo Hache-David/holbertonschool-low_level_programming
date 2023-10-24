972f42ba11ac1757b559814da5095f0b778e3296
root <146715401+Hache-David@users.noreply.github.com>
2023-10-24 00:35:05 -0700

    V8.1

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -13,11 +13,15 @@ void print_array(int *a, int n)
 {
 	int i = 0;
 
-	while (i < (n - 1))
+	if (n > 1)
 	{
-		printf("%d, ", a[i]);
-		i++;
-	}
-	printf("%d\n", a[i]);
-}
+		while (i < (n - 1))
+		{
+			printf("%d, ", a[i]);
+			i++;
+		}
+		printf("%d", a[i]);
 
+	}
+	printf("\n");
+}
