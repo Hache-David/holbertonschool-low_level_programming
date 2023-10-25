44bfda5f9464b53be8d30cb5c66ab2162ddf43ec
root <146715401+Hache-David@users.noreply.github.com>
2023-10-25 05:46:55 -0700

    Vtest

---
+++ pointers_arrays_strings/7-leet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * leet - check the code for
+ *
+ * @str : pointer.
+ *
+ * Return: Always 0.
+ */
+
+char *leet(char *str)
+{
+	int i = 0;
+
+	if (str[i] = 1)
+
+	else if (str[i] = 2)
+
+	return (str);
+}
