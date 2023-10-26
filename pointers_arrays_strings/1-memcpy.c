b15517b694b26bc31d4c919c5ec7d68398615ef7
root <146715401+Hache-David@users.noreply.github.com>
2023-10-26 05:04:26 -0700

    V1.0

---
+++ pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _memcpy - prints buffer in hexa.
+ *
+ * @dest: destination.
+ * @src: sourçe.
+ * @n: bytes.
+ *
+ * Return: char.
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; src[i] != '\0' && i < n ; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
