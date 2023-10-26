27e9977df17cba098d812315b3b42d8a554c6ffd
root <146715401+Hache-David@users.noreply.github.com>
2023-10-26 07:04:42 -0700

    V2.0

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strchr - check the code
+ *
+ * @s: string.
+ * @c: charactere.
+ *
+ * Return: Always 0.
+ */
+
+char *_strchr(char *s, char c)
+{
+	char *s2 = NULL;
+	int i;
+
+	for (i = 0 ; s[i] != '\0' ; i++)
+	{
+		if (s[i] == c)
+		{
+			s2 = &s[i];
+			return (s2);
+		}
+	}
+	return (NULL);
+}
