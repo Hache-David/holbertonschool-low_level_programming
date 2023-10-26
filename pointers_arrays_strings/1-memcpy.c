f124fc998d8c2ab3bc4143620b815783c19cfecf
root <146715401+Hache-David@users.noreply.github.com>
2023-10-26 05:31:07 -0700

    V1.1

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -14,9 +14,10 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
 
-	for (i = 0; src[i + 1] != '\0' && i < n ; i++)
+	for (i = 0; src[i] != '\0' && i < n ; i++)
 	{
 		dest[i] = src[i];
 	}
+	dest[i] = 0;
 	return (dest);
 }
