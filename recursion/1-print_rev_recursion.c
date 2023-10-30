d339934e87a2657cefe9f09cbb2a012de54d095e
root <146715401+Hache-David@users.noreply.github.com>
2023-10-30 03:12:45 -0700

    V1.0

---
+++ recursion/1-print_rev_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - check the code
+ *
+ * @s : string.
+ *
+ * Return: Always 0.
+ */
+
+void _print_rev_recursion(char *s)
+{
+	int i = 0;
+
+	if (s[i] != '\0')
+	{
+		_print_rev_recursion(&s[i + 1]);
+	}
+	_putchar(s[i]);
+}
