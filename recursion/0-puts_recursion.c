a42f544d554e0a90a9a6c7eab62da5191e0d980b
root <146715401+Hache-David@users.noreply.github.com>
2023-10-30 02:39:14 -0700

    V0.0

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - check the code
+ *
+ * @s: string.
+ *
+ * Return: Always 0.
+ */
+
+void _puts_recursion(char *s)
+{
+	int i = 0;
+
+	_putchar(s[i]);
+	i++;
+	if (s[i] != '\0')
+	{
+		_puts_recursion(&s[i]);
+	}
+	if (s[i] == '\0')
+	{
+		_putchar('\n');
+	}
+}
