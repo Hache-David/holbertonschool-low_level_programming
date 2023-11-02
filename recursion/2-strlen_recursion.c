a2eb32e7ba45b7dfe2afebc1739f94d4902d7215
root <146715401+Hache-David@users.noreply.github.com>
2023-11-02 04:10:58 -0700

    V2.0

---
+++ recursion/2-strlen_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - check the code
+ *
+ * @s: string.
+ *
+ * Return: Always 0.
+ */
+
+int _strlen_recursion(char *s)
+{
+	int i = 0;
+	int index = 0;
+
+	if (s[i + 1] != '\0')
+	{
+		index = index + _strlen_recursion(&s[i + 1]);
+	}
+	return (index + 1);
+}
