d1e8c8783267d22c5c7b3cbbe534b0023cc04558
root <146715401+Hache-David@users.noreply.github.com>
2023-11-02 02:59:53 -0700

    V0.2

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -4,12 +4,14 @@
  * main - Entry point.
  *
  * @argv: an array of pointers to the strings.
+ * @argc: argt count.
  *
  * Return: always 0.
  */
 
-int main(char **argv)
+int main(int argc, char **argv)
 {
+	argc = 1;
 	printf("%s\n", argv[0]);
 	return (argc);
 }
