9ef34af5b7c524ce93a581b50b8deb32f8d51c1e
root <146715401+Hache-David@users.noreply.github.com>
2023-11-02 05:25:15 -0700

    V2.2

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -13,6 +13,10 @@ int _strlen_recursion(char *s)
 	int i = 0;
 	int index = 0;
 
+	if (s[i] == '\0')
+	{
+		return (index);
+	}
 	if (s[i + 1] != '\0')
 	{
 		index = index + _strlen_recursion(&s[i + 1]);
