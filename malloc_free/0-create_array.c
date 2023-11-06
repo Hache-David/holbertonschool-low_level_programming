6aeca02bad7b29f36f299a78c82fc2f6723f5d36
root <146715401+Hache-David@users.noreply.github.com>
2023-11-06 02:18:53 -0800

    clean status

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * create_array - function that creates an array of chars.
+ * @c: char variable.
+ * @size: the size of the memory to print.
+ *
+ * Return: Nothing.
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *str;
+	unsigned int i;
+
+	str = malloc(sizeof(c) * size);
+	for (i = 0; i < size; i++)
+		str[i] = c;
+	if (size == 0)
+		return (NULL);
+	if (str == NULL)
+		return (NULL);
+	else
+		return (str);
+	free(str);
+}
