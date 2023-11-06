24f4251be6503fb249681adb06c0b7385921450b
root <146715401+Hache-David@users.noreply.github.com>
2023-11-06 02:34:35 -0800

    V1.0

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _strdup - check the code
+ *
+ * @str: string.
+ *
+ * Return: Always 0.
+ */
+
+char *_strdup(char *str)
+{
+	int size = 0;
+	char *str2;
+
+	if (str == NULL)
+		return (NULL);
+	while (str[size])
+		size++;
+	str2 = malloc(sizeof(char) * size);
+	if (str2 == NULL)
+		return (NULL);
+	for (size = 0; str[size] ; size++)
+	{
+		str2[size] = str[size];
+	}
+	return (str2);
+	free(str2);
+}
