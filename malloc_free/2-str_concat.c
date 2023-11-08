76601956ecea00799494b92933c58ee7788eb41b
root <146715401+Hache-David@users.noreply.github.com>
2023-11-08 06:05:15 -0800

    V2.0

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -15,28 +15,28 @@ char *str_concat(char *s1, char *s2)
 	char *s3;
 	int size1 = 0;
 	int size2 = 0;
-	int i, j;
-
+	int j = 0;
+	int i = 0;
+	
 	while (s1[size1])
 		size1++;
 	while (s2[size2])
 		size2++;
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 	s3 = malloc(sizeof(char) * (size1 + size2));
 	if (s3 == NULL)
-	{
-		free(s3);
 		return (NULL);
-	}
-	for (i = 0; i < size1; i++)
+	for (i = 0; s1[i]; i++)
 	{
 		s3[i] = s1[i];
 	}
-	s3[i] = s1[i];
-	for (j = 0; j < size2; j++)
+	for (j = 0; s2[j]; j++)
 	{
 		s3[i] = s2[j];
 		i++;
 	}
-	s3[i] = s2[j];
 	return (s3);
 }
