613bec6fb90e5d510a9d3d06edc27b732782329b
root <146715401+Hache-David@users.noreply.github.com>
2023-11-08 06:55:33 -0800

    V0.0

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+
+/**
+ * malloc_checked - check the code
+ *
+ * @b: size.
+ *
+ * Return: nothing.
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	unsigned int *str;
+
+	str = malloc(b);
+	if (str == NULL)
+	{
+		*str = 98;
+		return (str);
+	}
+	return (str);
+}
