d666ed2393455ea814690255712f2b2151ca607b
root <146715401+Hache-David@users.noreply.github.com>
2023-11-09 04:46:12 -0800

    V3.0

---
+++ more_malloc_free/3-array_range.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * array_range - prints buffer in hexa
+ * @min: minimum
+ * @max: maximum
+ *
+ * Return: Nothing.
+ */
+
+int *array_range(int min, int max)
+{
+	int i;
+	int *str;
+
+	str = malloc((max + 1) * sizeof(int));
+	if (str == NULL)
+		return (NULL);
+	for (i = 0; min <= max ; min++)
+		str[i++] = min;
+	return (str);
+}
+
