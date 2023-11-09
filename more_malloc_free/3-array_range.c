ff43820535aa248226d28fc3ed7c98f7a08db2f0
root <146715401+Hache-David@users.noreply.github.com>
2023-11-09 05:13:31 -0800

    V3.5

--- more_malloc_free/3-array_range.c
+++ more_malloc_free/3-array_range.c
@@ -16,7 +16,7 @@ int *array_range(int min, int max)
 
 	if (min > max)
 		return (NULL);
-	str = malloc((max + 1) * sizeof(int));
+	str = malloc(((max - min) + 1) * sizeof(int));
 	if (str == NULL)
 		return (NULL);
 	for (i = 0; min < max ; min++)
