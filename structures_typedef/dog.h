030caceff809225bbda86766e2f524fb67384e54
root <146715401+Hache-David@users.noreply.github.com>
2023-11-13 02:56:08 -0800

    V0.1

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,5 +1,21 @@
-struct dog{
+#ifndef STRUCTURE_TYPEDEF
+
+#define STRUCTURE_TYPEDEF
+
+#define dog
+/**
+ * struct dog - my first structure
+ *
+ * @name: name
+ * @age : age
+ * @owner : owner
+ *
+ * Description: it's my first struct
+ */
+struct dog
+{
 	char *name;
 	float age;
 	char *owner;
 };
+#endif
