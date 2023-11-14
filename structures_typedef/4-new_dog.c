742e70aaeb18a3d808a2a6b9ea12a898a9e8ff7d
root <146715401+Hache-David@users.noreply.github.com>
2023-11-14 01:24:57 -0800

    V4.3test

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -27,19 +27,15 @@ dog_t *new_dog(char *name, float age, char *owner)
 	if (new_dog == NULL)
 		return (NULL);
 	new_dog->name = malloc(sizeof(name) * (index1 + 1));
-	if (new_dog->name == NULL)
-	{
-		free(new_dog);
-		return (NULL);
-	}
 	new_dog->owner = malloc(sizeof(owner) * (index2 + 1));
-	if (new_dog->owner == NULL)
+	new_dog->age = age;
+	if (new_dog->name == NULL || new_dog->owner == NULL)
 	{
+		free(new_dog->owner);
 		free(new_dog->name);
 		free(new_dog);
 		return (NULL);
 	}
-	new_dog->age = age;
 	for (i = 0; i < index1 ; i++)
 		new_dog->name[i] = name[i];
 	for (i = 0; i < index2 ; i++)
