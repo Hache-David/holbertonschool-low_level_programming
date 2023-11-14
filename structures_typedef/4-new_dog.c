4bfeda75e3484ae4852f70bf57e832e93cff07fa
root <146715401+Hache-David@users.noreply.github.com>
2023-11-14 01:11:09 -0800

    V4.1

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -23,17 +23,24 @@ dog_t *new_dog(char *name, float age, char *owner)
 		index1++;
 	while (owner[index2])
 		index2++;
-	new_dog = malloc(sizeof(dog_t) * (index1 + index2));
+	new_dog = malloc(sizeof(dog_t));
 	if (new_dog == NULL)
 		return (NULL);
 	new_dog->name = malloc(sizeof(name) * (index1 + 1));
 	new_dog->owner = malloc(sizeof(owner) * (index2 + 1));
 	new_dog->age = age;
 	if (new_dog->name == NULL || new_dog->owner == NULL)
+	{
+		free(new_dog->name);
+		free(new_dog->owner);
+		free(new_dog);
 		return (NULL);
+	}
 	for (i = 0; i < index1 ; i++)
 		new_dog->name[i] = name[i];
 	for (i = 0; i < index2 ; i++)
 		new_dog->owner[i] = owner[i];
+	new_dog->name[index1] = '\0';
+	new_dog->owner[index2] = '\0';
 	return (new_dog);
 }
