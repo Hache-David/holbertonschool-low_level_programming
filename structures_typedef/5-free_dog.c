03282ecdce6c86538fbca44ea22e1efed60a6d31
root <146715401+Hache-David@users.noreply.github.com>
2023-11-14 02:03:50 -0800

    V5.0

---
+++ structures_typedef/5-free_dog.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * free_dog - check the code
+ *
+ * @d : pointer in dog_t
+ *
+ * Return: Always 0.
+ */
+
+void free_dog(dog_t *d)
+{
+	free(d->name);
+	free(d->owner);
+	free(d);
+}
