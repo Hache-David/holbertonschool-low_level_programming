50d259ecb3326071f1ebed8f2108f5bc597d8437
Hache-David <hache.david@free.fr>
2023-11-17 13:40:13 +0000

    drfgd

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -26,7 +26,6 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 			printf("%d", va_arg(args, int));
 	}
 	printf("%d", va_arg(args, int));
-	printf("\n");
-
 	va_end(args);
+	printf("\n");
 }
