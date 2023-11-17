06983907cdee039a4f79cb1f5c9e8b74226dc783
Hache-David <hache.david@free.fr>
2023-11-17 13:28:12 +0000

    V1.3

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -19,7 +19,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_start(args, n);
 
 	if (n == 0)
-		return (0);
+		return;
 	for (i = 0; i < (n - 1); i++)
 	{
 		if (separator != NULL)
