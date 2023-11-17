cf28595f469ff3083234ceb38b9cae6ac96968fd
Hache-David <hache.david@free.fr>
2023-11-17 13:43:15 +0000

    lk!jkl

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -18,14 +18,13 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 
 	va_start(args, n);
 
-	for (i = 0; i < (n - 1); i++)
+	for (i = 0; i < n; i++)
 	{
-		if (separator != NULL)
+		if (i < (n - 1) && separator != NULL)
 			printf("%d%s", va_arg(args, int), separator);
 		else
 			printf("%d", va_arg(args, int));
 	}
-	printf("%d", va_arg(args, int));
 	va_end(args);
 	printf("\n");
 }
