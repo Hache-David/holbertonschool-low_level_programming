1e578dc3ec5dcf7cfb092fbcf4ad88c94ef04f72
Hache-David <hache.david@free.fr>
2023-11-27 14:22:49 +0000

    Add readme for project 1 (singly_linked_list). Add the first exercice of project 1 (print_list). Add list.h the header folder of project 1.

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_list - print an element of the list create in main.h
+ *
+ * @h: pointer on structure list_s.
+ *
+ * Description: singly linked list node structure
+ *
+ * Return: number of the elements in the list.
+ */
+
+size_t print_list(const list_t *h)
+{
+	unsigned long size = 0;
+
+	if (h->str == NULL)
+	{
+		printf("[0] %s\n", h->str);
+		size++;
+		h = h->next;
+	}
+	while (h != NULL)
+	{
+		printf("[%d] %s\n", h->len, h->str);
+		size++;
+		h = h->next;
+	}
+
+	return (size);
+}
---
+++ singly_linked_lists/README.md
@@ -0,0 +1,5 @@
+## Exercise directory allowing you to manipulate linked lists:
+
+-print_list : print an element of the list create in main.h.
+
+-
