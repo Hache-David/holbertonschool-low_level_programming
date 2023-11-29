31a88b6af16a3b908c10338388ff9b0ccdf35bc9
Hache-David <hache.david@free.fr>
2023-11-29 13:59:56 +0000

    Add the folder for task 3 in project 2. Fix the header file with new prototype.

---
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -0,0 +1,40 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * add_dnodeint_end - add new node in the back of the list
+ *
+ * @head: pointer to the node.
+ * @n: the integrate value in the node.
+ *
+ * Return: the address of the new node.
+ */
+
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+	dlistint_t *new_node = NULL;
+
+	new_node = malloc(sizeof(dlistint_t));
+
+	if (new_node == NULL)
+		return (new_node);
+	new_node->n = n;
+	new_node->next = NULL;
+	if (*head == NULL)
+		(*head) = new_node;
+	else
+	{
+		dlistint_t *tail = (*head);
+
+		while (tail->next != NULL)
+		{
+			tail = tail->next;
+		}
+		tail->next = new_node;
+	}
+	if (*head != NULL)
+		(*head)->prev = new_node;
+	return (new_node);
+}
