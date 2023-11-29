cbc8233524108f7814907afe32a41d41f952a81e
Hache-David <hache.david@free.fr>
2023-11-29 11:40:43 +0000

    Fix the code for a checker issue .

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -14,13 +14,12 @@
 
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-	dlistint_t *new_node;
+	dlistint_t *new_node = NULL;
 
 	new_node = malloc(sizeof(dlistint_t));
-	if(head == NULL)
-		return (NULL);
+
 	if (new_node == NULL)
-		return (NULL);
+		return (new_node);
 	new_node->n = n;
 	new_node->next = *head;
 
