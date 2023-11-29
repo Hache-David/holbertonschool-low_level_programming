bbafaf3cb4ce3d9acf211ea958d9b104dd43b9db
Hache-David <hache.david@free.fr>
2023-11-29 10:57:29 +0000

    Add the folder for the task 1 in project 2. Add the header file of the 2nd project. Add readme folder of the 2nd project

---
+++ doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_dlistint - print all the element of the list
+ *
+ * @h: the first node of the list.
+ *
+ * Return: index the number of node in the list.
+ */
+
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t index = 0;
+
+	if (h == NULL)
+	{
+		return (0);
+	}
+	while (h->next)
+	{
+		printf("%d\n", h->n);
+		index++;
+		h = h->next;
+	}
+	printf("%d\n", h->n);
+	return (index);
+}
