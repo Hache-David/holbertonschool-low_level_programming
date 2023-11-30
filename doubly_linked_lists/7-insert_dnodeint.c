5a9bfde799272c6b0dd8fadd40e242adcbe47bda
Hache-David <hache.david@free.fr>
2023-11-30 12:47:43 +0000

    Add new version of exercice 7

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -36,9 +36,10 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		{
 			copyh->prev = temp->prev;
 			temp->prev = copyh;
+			return (copyh);
 		}
 		index++;
 		temp = temp->next;
 	}
-	return (copyh);
+	return (NULL);
 }
