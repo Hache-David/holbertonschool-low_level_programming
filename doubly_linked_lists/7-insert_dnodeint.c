9882363f44874074bec6919b819ecc6068f01bfd
Hache-David <hache.david@free.fr>
2023-11-30 13:02:51 +0000

    retest

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -36,7 +36,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	}
 	while (index <= (idx + 1))
 	{
-		if (temp->next == NULL)
+		if (temp == NULL)
 		{
 			return (NULL);
 		}
