b2b64fc1cffb966ab76a3d25c154d9325982f465
Hache-David <hache.david@free.fr>
2023-11-30 09:37:37 +0000

    fix issue

--- doubly_linked_lists/3-add_dnodeint_end.c
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -33,7 +33,7 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 			tail = tail->next;
 		}
 		tail->next = new_node;
-		(*head)->prev = new_node;
+		tail->prev = new_node;
 	}
 	return (new_node);
 }
