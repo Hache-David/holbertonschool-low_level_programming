eecb234ce5afc4fbd35954e5e56e1c67b1a260b8
Hache-David <hache.david@free.fr>
2023-11-30 09:44:56 +0000

    Fix checker issue

--- doubly_linked_lists/3-add_dnodeint_end.c
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -15,25 +15,29 @@
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
 	dlistint_t *new_node;
+	dlistint_t *tail;
 
 	new_node = malloc(sizeof(dlistint_t));
 
 	if (new_node == NULL)
 		return (NULL);
+
+	tail = malloc(sizeof(dlistint_t));
+
 	new_node->n = n;
 	new_node->next = NULL;
 	if (*head == NULL)
-		(*head) = new_node;
-	else
 	{
-		dlistint_t *tail = (*head);
-
-		while (tail->next != NULL)
-		{
-			tail = tail->next;
-		}
-		tail->next = new_node;
-		tail->prev = new_node;
+		new_node->prev = NULL;
+		(*head) = new_node;
+		return (new_node);
 	}
+	tail = *head;
+	while (tail->next != NULL)
+	{
+		tail = tail->next;
+	}
+	tail->next = new_node;
+	new_node->prev = tail;
 	return (new_node);
 }
