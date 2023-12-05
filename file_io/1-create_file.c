91794b3faccb9a1c5c20dda6876e416c9807f9bd
Hache-David <hache.david@free.fr>
2023-12-05 14:08:48 +0000

    Retest checker

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -20,13 +20,16 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	while (text_content[length])
-		length++;
+
 	descripteur = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (descripteur == -1)
 		return (-1);
 	if (text_content != NULL)
+	{
+		while (text_content[length])
+			length++;
 		check_error = write(descripteur, text_content, length);
+	}
 	if (check_error == -1)
 		return (-1);
 	close(descripteur);
