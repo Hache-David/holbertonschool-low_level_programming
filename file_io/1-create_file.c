a3ff551eef43f8c220f286ad3e1da53a6b2dccd6
Hache-David <hache.david@free.fr>
2023-12-05 13:36:54 +0000

    Fix betty style on exercice 1.

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -19,7 +19,7 @@ int create_file(const char *filename, char *text_content)
 	int length = 0;
 
 	if (filename == NULL)
-		return(-1);
+		return (-1);
 	while (text_content[length])
 		length++;
 	descripteur = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
