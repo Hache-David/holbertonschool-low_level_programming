516113081485ccb78e3efae8f544db22567b89c6
Hache-David <hache.david@free.fr>
2023-12-06 14:53:26 +0000

    sfdecs

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -20,17 +20,22 @@ int main(int argc, char **argv)
 	check_error, check_error2, rd;
 	char str[1024];
 
-	descripteur = open(argv[1], O_RDONLY);
-	if (descripteur == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[2]);
-		exit(98);
-	}
-	empty_folder = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (argc != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
+	}
+	descripteur = open(argv[1], O_RDONLY);
+	if (descripteur == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+	empty_folder = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	if (empty_folder == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		exit(99);
 	}
 	bytes_read = read(descripteur, str, 1024);
 	if (bytes_read == -1)
