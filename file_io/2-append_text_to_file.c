e172e7a59f7613ce1784fd0a531b4f8663d10d8b
Hache-David <hache.david@free.fr>
2023-12-06 08:57:12 +0000

    Add file for exercice 2 project 1. Fix header file with new prototype.

---
+++ file_io/2-append_text_to_file.c
@@ -0,0 +1,38 @@
+#include <stdlib.h>
+#include "main.h"
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+/**
+ * create_file - create and write on it .
+ *
+ * @text_content: the text write on the file.
+ * @filename: name of the file.
+ *
+ * Return: 1 if success, -1 if error.
+ */
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+		int descripteur, check_error;
+	int length = 0;
+
+	if (filename == NULL)
+		return (-1);
+
+	descripteur = open(filename,O_WRONLY | O_APPEND, 0600);
+	if (descripteur == -1)
+		return (-1);
+	if (text_content != NULL)
+	{
+		while (text_content[length])
+			length++;
+		check_error = write(descripteur, text_content, length);
+	}
+	if (check_error == -1)
+		return (-1);
+	close(descripteur);
+	return (1);
+}
+
