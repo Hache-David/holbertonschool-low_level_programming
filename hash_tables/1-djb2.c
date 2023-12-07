6f9c01a8e50c46026bb54b7b3f7d782dd63702e6
Hache-David <hache.david@free.fr>
2023-12-07 11:52:32 +0000

    Add folder for exercice 1 project 2. Fix header file with new prototype.

---
+++ hash_tables/1-djb2.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "hash_tables.h"
+
+/**
+ * hash_djb2 - simulate the djb2 hash algorythme .
+ *
+ * @str: input string.
+ *
+ * Return: the hash .
+ */
+
+unsigned long int hash_djb2(const unsigned char *str)
+{
+	unsigned long int hash;
+	int c;
+
+	hash = 5381;
+	while ((c = *str++))
+	{
+		hash = ((hash << 5) + hash) + c;
+	}
+	return (hash);
+}
