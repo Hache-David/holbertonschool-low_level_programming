edbead8d296974cbe9d0efab488909d73350e7cf
Hache-David <hache.david@free.fr>
2023-12-07 10:56:09 +0000

    Add exercice 0 for project 2. Add header file for project 2. Add readme folder for project 2.

---
+++ hash_tables/0-hash_table_create.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - create an hash table .
+ *
+ * @size: size of hash table.
+ *
+ * Return: hash table.
+ */
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *hashtable;
+
+	hashtable = malloc(sizeof(hash_table_t));
+	if (hashtable == NULL)
+		return (NULL);
+	hashtable->size = size;
+	hashtable->array = calloc(size, sizeof(hash_node_t *));
+	return (hashtable);
+}
