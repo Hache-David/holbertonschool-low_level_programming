faeeaba0bf6b5676edcb6ffbe20d0af1f25d5b50
Hache-David <hache.david@free.fr>
2023-12-08 14:57:34 +0000

    Add last exercice of project 2. Fix header file with new prototype.

---
+++ hash_tables/6-hash_table_delete.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include "hash_tables.h"
+
+/**
+ * hash_table_delete - Deletes a hash table
+ *
+ * @ht: The hash table
+ *
+ * Return: nothing.
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned long int i;
+	hash_node_t *current;
+	hash_node_t *temp;
+
+	if (ht == NULL)
+		return;
+	for (i = 0; i < ht->size; ++i)
+	{
+		current = ht->array[i];
+		while (current != NULL)
+		{
+			temp = current;
+			current = current->next;
+			free(temp->key);
+			free(temp->value);
+			free(temp);
+		}
+	}
+	free(ht->array);
+	free(ht);
+}
+
